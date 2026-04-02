40b915058eafeb79264a5996ab158e4537262db8
rohankumar110 <rohanktalreja110@gmail.com>
2026-04-01 22:34:15 -0400

    make home-details page mobile responsive

--- src/app/hotel-details/index.jsx
+++ src/app/hotel-details/index.jsx
@@ -13,18 +13,20 @@ function HotelDetails() {
   return (
     <div className="container mt-6 mb-12">
       <PropertyViewCarousel images={HOTEL_DATA.hotel.photos} />
-      <div className="flex gap-6 mt-6">
-        <div className="flex-1 space-y-8">
-          <HotelMetaDetails hotel={hotelData.hotel} hotelInfo={hotelInfo} />
-          <HotelRoomPicker rooms={hotelData.rooms} />
-          <HotelPolicy hotelPolicy={hotelInfo.hotelPolicy} />
-        </div>
-        <aside className="sticky top-6 w-85 h-min shrink-0 p-4 border border-border shadow-md rounded-xl">
+
+      <div className="mt-6 flex flex-col gap-6 lg:flex-row">
+        <aside className="order-1 w-full rounded-xl border border-border p-4 shadow-md lg:order-2 lg:sticky lg:top-6 lg:h-min lg:w-85 lg:shrink-0">
           <HotelCheckoutCard
             rooms={hotelData.rooms}
             cancellationPolicy={hotelInfo.cancellationPolicy}
           />
         </aside>
+
+        <div className="order-2 space-y-8 lg:order-1 lg:flex-1">
+          <HotelMetaDetails hotel={hotelData.hotel} hotelInfo={hotelInfo} />
+          <HotelRoomPicker rooms={hotelData.rooms} />
+          <HotelPolicy hotelPolicy={hotelInfo.hotelPolicy} />
+        </div>
       </div>
     </div>
   );
