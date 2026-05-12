c0d68a09fe448208f9af2bfabdde1d274fedf01d
rohankumar110 <rohanktalreja110@gmail.com>
2026-05-11 22:00:17 -0400

    add path config

--- src/app/router.jsx
+++ src/app/router.jsx
@@ -6,17 +6,19 @@ import { SignInPage, SignUpPage } from "./auth";
 import { BrowserRouter, Route, Routes } from "react-router";
 import Header from "@/components/layouts/header.layout";
 import Footer from "@/components/layouts/footer.layout";
+import PATHS from "@/config/path.config";
 
 function Router() {
+  
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signin" element={<SignInPage />} />
-        <Route path="signup" element={<SignUpPage />} />
-        <Route path="/search" element={<SearchPage />} />
-        <Route path="/hotels/:id" element={<HotelDetails />} />
+        <Route path={PATHS.LANDING} element={<Home />} />
+        <Route path={PATHS.SIGN_IN} element={<SignInPage />} />
+        <Route path={PATHS.SIGN_UP} element={<SignUpPage />} />
+        <Route path={PATHS.SEARCH_HOTEL} element={<SearchPage />} />
+        <Route path={PATHS.HOTEL_DETAIL} element={<HotelDetails />} />
       </Routes>
       <Footer />
     </BrowserRouter>
